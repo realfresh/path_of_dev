a17ea8ac6816f47162da5892095405400fa85e34
realfresh <cpatarun@gmail.com>
2019-08-12 15:54:30 +0800

    fix react-tag-input styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,3 @@
 import "./src/styles/index.scss"
-import "@pathofdev/react-tag-input/build/index.css"
+// import "@pathofdev/react-tag-input/src/styles/index.scss"
+require("@pathofdev/react-tag-input/build/index.css")
